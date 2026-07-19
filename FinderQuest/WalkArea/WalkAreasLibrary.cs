1b97c35f64dca68d2d171e833805e180f20069e4
ZEPHYR1702 <keffkaunderage@gmail.com>
2026-07-19 18:33:37 +0700

    added npc generator

--- FinderQuest/WalkArea/WalkAreasLibrary.cs
+++ FinderQuest/WalkArea/WalkAreasLibrary.cs
@@ -17,8 +17,8 @@ namespace FinderQuest.WalkArea
             {
                 case 1:
                     createWalkArea = new WalkAreas("Floor 1", Properties.Resources.floor1, 1);
-                    createWalkArea.AddPerson(1, "Clone 1", Properties.Resources.npc_back, new Point(150, 350), "hi");
-                    createWalkArea.AddPerson(2, "Clone 2", Properties.Resources.npc_front, new Point(420, 350), "hi");
+                    createWalkArea.AddPerson(1, "Clone 1", Properties.Resources.npc_front, new Point(20, 100), "hi");
+                    createWalkArea.AddPerson(2, "Clone 2", Properties.Resources.npc_front, new Point(420, 280), "hi");
                     createWalkArea.AddPerson(3, "Clone 3", Properties.Resources.npc_back, new Point(600, 360), "hi");
                     break;
                 case 2:
